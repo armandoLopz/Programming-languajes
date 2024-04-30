3785d15e9034966e375cbdb12c2968afd24208b2
armandoLopz <armandolopez0611@gmail.com>
2024-04-30 10:42:31 -0400

    Reestructure folders

---
+++ 1evaluation/main.js
@@ -0,0 +1,26 @@
+function mostrarFormulario2() {
+    document.getElementById('form_user').classList.add('hidden');
+    document.getElementById('form_car').classList.remove('hidden');
+}
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+    event.preventDefault();
+    mostrarFormulario2();
+});
+
+function mostrarFormulario1() {
+    document.getElementById('form_car').classList.add('hidden');
+    document.getElementById('form_user').classList.remove('hidden');
+}
+
+document.getElementById('atras').addEventListener('click', function() {
+    event.preventDefault();
+    mostrarFormulario1();
+});
+
+document.getElementById('add').addEventListener('click',function(){
+    event.preventDefault();
+    window.location.href="formCar.html";
+
+
+});
