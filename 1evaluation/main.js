0a8f6c712500ce2af5d4e4e37ca2683137f98a2a
Diego22gd <d.valdemar16@gmail.com>
2024-04-30 19:39:14 -0400

    cambio
    cree un js  para el formulario y uno para el main

--- 1evaluation/main.js
+++ 1evaluation/main.js
@@ -1,24 +1,4 @@
-function mostrarFormulario2() {
-    document.getElementById('form_user').classList.add('hidden');
-    document.getElementById('form_car').classList.remove('hidden');
-}
 
-document.getElementById('nextBtn').addEventListener('click', function() {
-    event.preventDefault();
-
-
-    mostrarFormulario2();
-});
-
-function mostrarFormulario1() {
-    document.getElementById('form_car').classList.add('hidden');
-    document.getElementById('form_user').classList.remove('hidden');
-}
-
-document.getElementById('atras').addEventListener('click', function() {
-    event.preventDefault();
-    mostrarFormulario1();
-});
 
 document.getElementById('add').addEventListener('click',function(){
     event.preventDefault();
