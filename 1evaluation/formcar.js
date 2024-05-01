fae97650e15333728849085d77dd11479a880eec
armandoLopz <armandolopez0611@gmail.com>
2024-05-01 11:33:22 -0400

    hh

--- 1evaluation/formcar.js
+++ 1evaluation/formcar.js
@@ -18,4 +18,29 @@ function mostrarFormulario1() {
 document.getElementById('atras').addEventListener('click', function() {
     event.preventDefault();
     mostrarFormulario1();
+});import './validation.js';
+
+function mostrarFormulario2() {
+    document.getElementById('form_user').classList.add('hidden');
+    document.getElementById('form_car').classList.remove('hidden');
+}
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+    
+    event.preventDefault(); 
+
+    mostrarFormulario2();
 });
+
+function mostrarFormulario1() {
+    document.getElementById('form_car').classList.add('hidden');
+    document.getElementById('form_user').classList.remove('hidden');
+}
+
+document.getElementById('atras').addEventListener('click', function() {
+    event.preventDefault();
+    mostrarFormulario1();
+});
+
+
+const validation = new va
