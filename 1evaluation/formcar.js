fd487eba2665e6fa8cd8c465ab29ab86bc236155
armandoLopz <armandolopez0611@gmail.com>
2024-05-03 10:44:19 -0400

    Add function for add elements in table

--- 1evaluation/formcar.js
+++ 1evaluation/formcar.js
@@ -1,4 +1,5 @@
 import { person, car, validationFormUser,validationFormCar } from './validation.js';
+//import { tableDom } from './main.js';
 
 const user = new person();
 const car1 = new car();
@@ -6,6 +7,10 @@ const car1 = new car();
 const validationUser = new validationFormUser(user);
 const validationCar = new validationFormCar(car1);
 
+const form_car = document.getElementById("form2");
+
+
+const tableListCars = localStorage.getItem("table");
 
 function mostrarFormulario1() {
 
@@ -15,8 +20,42 @@ function mostrarFormulario1() {
 
 function mostrarFormulario2(ownerCar) {
 
+    //Se asigna el propietario del carro
+    car1.ownerCar = ownerCar;
+    
     document.getElementById('form_user').classList.add('hidden');
     document.getElementById('form_car').classList.remove('hidden');
+}
+
+function addElementsTable(car) {
+
+    const fila = document.createElement('tr');
+    
+    const celdaCedula = document.createElement('td');
+    celdaCedula.textContent = car.idCar;
+    fila.appendChild(celdaCedula);
+    
+    const celdaYear = document.createElement('td');
+    celdaYear.textContent = car.yearCar;
+    fila.appendChild(celdaYear);
+
+    const celdaModelo = document.createElement('td');
+    celdaModelo.textContent = car.brandCar;
+    fila.appendChild(celdaModelo);
+
+    const celdaFoto = document.createElement('td');
+    celdaFoto.textContent = car.photoCar;
+    fila.appendChild(celdaFoto);
+
+    const celdaColor = document.createElement('td');
+    celdaColor.textContent = car.colorCar;
+    fila.appendChild(celdaColor);
+
+    
+    // Agrega la fila a la tabla
+    tableListCars.appendChild(fila)
+    //Se muestra la pag principal
+    form_car.submit()
 }
 
 document.getElementById('nextBtn').addEventListener('click', function() {
@@ -29,12 +68,27 @@ document.getElementById('nextBtn').addEventListener('click', function() {
     user.phoneNumber = document.getElementById("phone").value;
     user.addressUser = document.getElementById("address").value;
 
-    const correctData  = validationUser.validationAllAtributes()
+    const correctDataUser  = validationUser.validationAllAtributes()
     
-    correctData == true ? mostrarFormulario2(user) : alert("Verifique sus datos");
+    correctDataUser == true ? mostrarFormulario2(user) : alert("Verifique sus datos");
 });
 
 document.getElementById('atras').addEventListener('click', function() {
     event.preventDefault();
     mostrarFormulario1();
+});
+
+form_car.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    car1.idCar = document.getElementById("idCar").value;
+    car1.yearCar = document.getElementById("yearCar").value;
+    car1.brandCar = document.getElementById("brandCar").value;
+    car1.colorCar = document.getElementById("colorCar").value;
+    car1.photoCar = document.getElementById("photoCar").value;
+
+    const correctDataCar = validationCar.validationAllAtributes()
+
+    correctDataCar == true ? addElementsTable(car1) : alert("Verifique sus datos"); 
+    
 });
