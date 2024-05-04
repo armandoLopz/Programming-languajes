549f95345a42afdd1d998730cf1f59d5db27f21f
Diego22gd <d.valdemar16@gmail.com>
2024-05-04 03:55:33 -0400

    cambios diseno
    hice algunos cambios en el diseno revisalos tengo que cambiar los botones aun pero revisa la tabal y el formulario termina las validaciones y que se agruegue a la tabla para continuar

--- 1evaluation/main.js
+++ 1evaluation/main.js
@@ -1,7 +1,42 @@
 const tableDom = document.getElementById("inventario");
 localStorage.setItem("table", tableDom);
+function mostrarFormulario1() {
+
+    document.getElementById('form_user').classList.remove('hidden');
+    document.getElementById('menu').classList.add('hidden');
+    document.getElementById('table').classList.add('hidden');
+}
 
 document.getElementById('add').addEventListener('click',function(){
     event.preventDefault();
-    window.location.href="formCar.html";
+    mostrarFormulario1();
+   
 });
+function mostrarFormulario2() {
+
+    document.getElementById('form_car').classList.remove('hidden');
+    document.getElementById('form_user').classList.add('hidden');
+   
+}
+document.getElementById('nextBtn').addEventListener('click',function(){
+ event.preventDefault();
+ mostrarFormulario2();
+
+
+
+
+})
+function regresar() {
+
+    document.getElementById('form_user').classList.remove('hidden');
+    document.getElementById('form_car').classList.add('hidden');
+   
+}
+document.getElementById('atras').addEventListener('click',function(){
+    event.preventDefault();
+    regresar();
+   
+   
+   
+   
+   })
