b26fb7b62db930f2ece29c37834769f01d59a52a
armandoLopz <armandolopez0611@gmail.com>
2024-07-18 06:19:58 -0400

    Fix routes

---
+++ 3valuation/programmingLenguajes/views/src/app/buttons/edit-button/edit-dialog/edit-dialog.component.ts
@@ -0,0 +1,65 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { Result } from '../../../models/book.models';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-dialog',
+  template: `
+    <h3 mat-dialog-title>Editar Elemento</h3>
+    <div mat-dialog-content>
+      <mat-form-field>
+        <mat-label>Autor</mat-label>
+        <input matInput [(ngModel)]="data.authors[0].name">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Libro</mat-label>
+        <input matInput [(ngModel)]="data.title">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Fecha de Nacimiento</mat-label>
+        <input matInput [(ngModel)]="data.authors[0].birth_year" type="number">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Fecha de Muerte</mat-label>
+        <input matInput [(ngModel)]="data.authors[0].death_year" type="number">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Genero</mat-label>
+        <input matInput [(ngModel)]="data.subjects[0]">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Descargas</mat-label>
+        <input matInput [(ngModel)]="data.download_count" type="number">
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Lenguaje</mat-label>
+        <input matInput [(ngModel)]="data.languages[0]">
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions class="botones">
+      <button mat-button (click)="onCancel()">Cancelar</button>
+      <button mat-button (click)="onSave()">Guardar</button>
+    </div>
+  `,
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule],
+  styleUrls: ['./edit-dialog.component.css']
+})
+export class EditDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<EditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Result
+  ) {}
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSave(): void {
+    this.dialogRef.close(this.data);
+  }
+}
---
+++ 3valuation/programmingLenguajes/views/src/app/filter-interface/buttons/add-button/add-button.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddDialogComponent } from './add-dialog/add-dialog.component';
+import { Result } from '../../../models/book.models';
+
+@Component({
+  selector: 'app-add-button',
+  template: `<button mat-button (click)="openAddDialog()">Agregar</button>`,
+  standalone: true,
+  styleUrl:'./add-button.component.css'
+  
+})
+export class AddButtonComponent {
+  @Output() newElement = new EventEmitter<Result>();
+
+  constructor(public dialog: MatDialog) {}
+
+  openAddDialog(): void {
+    const dialogRef = this.dialog.open(AddDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.newElement.emit(result);
+      }
+    });
+  }
+}
+
