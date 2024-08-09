449ab9ca92720ce4f36bd1b092b324df52786ed3
armandoLopz <armandolopez0611@gmail.com>
2024-08-09 08:52:36 -0400

    ds

--- 3valuation/programmingLenguajes/views/src/app/models/booksLaravel.models.ts
+++ 3valuation/programmingLenguajes/views/src/app/models/booksLaravel.models.ts
@@ -1,11 +1,2 @@
-export interface Book {
-    id?:        number;
-    title:     string;
-    author:    string;
-    bornDay:   number;
-    deathDate: number;
-    genre:     string;
-    downloads: number;
-}
 
 
